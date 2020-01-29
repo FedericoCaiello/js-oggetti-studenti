d171bbc7773c6b8bb82fed4f5c42bfb083e76b1c
Federico <federico.caiello@gmail.com>
2020-01-29 18:17:10 +0100

    secondo comando

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
-var utente = {
-  'nome': 'federico',
-  'cognome': 'caiello',
-  'eta\'': 25,
-};
-for (var caratteristiche in utente) {
-  console.log(utente[caratteristiche]);
+// var utente = {
+//   'nome': 'Federico',
+//   'cognome': 'Caiello',
+//   'eta\'': 25,
+// };
+// for (var caratteristiche in utente) {
+//   console.log(utente[caratteristiche]);
+// }
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var studenti = [
+{
+  'nome': 'Federico',
+  'cognome': 'Caiello',
+  'cose': 'macchina'
+},
+{
+  'nome': 'Marco',
+  'cognome': 'Stefanini',
+  'cose': 'macchina'
+},
+{
+  'nome': 'Lorenzo',
+  'cognome': 'Petrollini',
+  'cose': 'chiavi'
+}
+];
+console.log(studenti);
+
+for (var i=0; i < studenti.length; i++){
+  console.log(studenti[i].nome + studenti[i].cognome);
 }
 
-
-// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
